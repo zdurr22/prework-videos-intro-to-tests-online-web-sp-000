e627b73be345b3a6ce4b3447d36f0bde7487e6de
Zachary Durr <zdurr22@gmail.com>
2020-09-11 00:47:37 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -1,8 +1,8 @@
 require_relative './conversions.rb'
 
 puts "Given 0, it should return 0.0"
-answer = ounces_to_grams(0)
-if answer == 0.0
+answer = ounces_to_grams(1)
+if answer == 28.3495
   puts "Passed!"
 else
   puts "Test failed: got #{answer} instead"
